6ef2d60e2970d7f77ca6e0bb00c563a35203085b
Rafael Aguiar <rafael.a1@hotmail.com>
2022-11-25 16:30:05 -0300

    feat: insert launchs in DB

---
+++ src/utils/launches/insertLaunchesInBD.ts
@@ -0,0 +1,22 @@
+import { LoadLaunchesFromApi } from './loadLaunchesFromApiData'
+import { LaunchMongoRepository } from '../../infra/db/mongodb/launch-repository/lauch'
+import env from '../../main/config/env'
+
+export class InsertLaunchsInDB {
+  async run (): Promise<void> {
+    const launchesLink = env.launchesLink
+    const loadLaunchesFromAPI = new LoadLaunchesFromApi()
+    const launchMongoRepository = new LaunchMongoRepository()
+
+    // Checar se os dados estão atulizados
+    const launches = await loadLaunchesFromAPI.getLaunches(launchesLink)
+    launches.forEach(async (launchItem) => {
+      const launchAlreadyExists = await launchMongoRepository.findOne(launchItem.id)
+      if (launchAlreadyExists) {
+        console.log('LaunchAlreadyExists')
+      } else {
+        await launchMongoRepository.add(launchItem)
+      }
+    })
+  }
+}
