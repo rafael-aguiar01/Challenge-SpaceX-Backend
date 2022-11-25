2a14c1566a650cba7e7c54736cea7ebf103dcced
Rafael Aguiar <rafael.a1@hotmail.com>
2022-11-25 14:58:45 -0300

    refactor: connect mongoHelper in server.ts

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,3 +1,9 @@
+import { MongoHelper } from '../infra/db/mongodb/helpers/mongo-helper'
 import app from './config/app'
+import env from './config/env'
 
-app.listen(5050, () => console.log(`Server running at http://localhost:${5050}`))
+MongoHelper.connect(env.mongoUrl)
+  .then(async () => {
+    app.listen(env.port, () => console.log(`Server running at http://localhost:${5050}`))
+  })
+  .catch(console.error)
