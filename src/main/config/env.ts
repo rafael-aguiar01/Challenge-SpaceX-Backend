c81f719b442b432a56c8df50c781305115d20a6b
Rafael Aguiar <rafael.a1@hotmail.com>
2022-11-25 10:08:07 -0300

    chore: add mongodb

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,6 @@
 export default {
   rocketsLink: 'https://api.spacexdata.com/v4/rockets',
-  launchesLink: 'https://api.spacexdata.com/v5/launches'
+  launchesLink: 'https://api.spacexdata.com/v5/launches',
+  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/bettingfifaworldcup',
+  port: process.env.PORT || 5050
 }
