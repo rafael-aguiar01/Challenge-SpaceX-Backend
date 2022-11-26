3fcae85c554674e3b3e3b9483d2ac5f8350b67c7
Rafael Aguiar <rafael.a1@hotmail.com>
2022-11-26 13:14:05 -0300

    fix: addLaunchMongoRepository in factory

--- src/main/factories/find-launches.ts
+++ src/main/factories/find-launches.ts
@@ -1,6 +1,8 @@
+import { LaunchMongoRepository } from '../../infra/db/mongodb/launch-repository/lauch'
 import { FindLaunchesController } from '../../presentation/controllers/launches/find-launches-controller'
 import { Controller } from '../../presentation/protocols/controller'
 
 export const makeFindLaunchesController = (): Controller => {
-  return new FindLaunchesController()
+  const launchMongoRepository = new LaunchMongoRepository()
+  return new FindLaunchesController(launchMongoRepository)
 }
