ce651d7b66007b1db87e85328e53672eb07b3275
Rafael Aguiar <rafael.a1@hotmail.com>
2022-11-26 13:15:11 -0300

    feat: add try/catch in controller

--- src/presentation/controllers/launches/find-launches-controller.ts
+++ src/presentation/controllers/launches/find-launches-controller.ts
@@ -1,10 +1,22 @@
+import { FindLaunch } from '../../../domain/usecases/find-launch'
+import { ok, serverError } from '../../helpers/http-helper'
 import { Controller } from '../../protocols/controller'
 import { HttpRequest, HttpResponse } from '../../protocols/http'
 
 export class FindLaunchesController implements Controller {
+  private readonly findLaunch: FindLaunch
+
+  constructor (findLaunch: FindLaunch) {
+    this.findLaunch = findLaunch
+  }
+
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const { search, limit } = httpRequest.query
-    console.log(search, limit)
-    return null
+    try {
+      const { search, limit } = httpRequest.query
+      const launches = await this.findLaunch.find({ search, limit })
+      return ok(launches)
+    } catch (error) {
+      return serverError(error)
+    }
   }
 }
