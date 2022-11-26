ecffa1fc49a39ce2a79a996b0d2f26ab0020f672
Rafael Aguiar <rafael.a1@hotmail.com>
2022-11-26 13:11:49 -0300

    refactor: add helper to return

---
+++ src/presentation/helpers/http-helper.ts
@@ -0,0 +1,12 @@
+import { HttpResponse } from '../protocols'
+import { ServerError } from '../errors/server-error'
+
+export const serverError = (error: Error): HttpResponse => ({
+  statusCode: 500,
+  body: new ServerError(error.stack)
+})
+
+export const ok = (data: any): HttpResponse => ({
+  statusCode: 200,
+  body: data
+})
