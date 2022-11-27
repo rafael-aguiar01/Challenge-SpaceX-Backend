0f67695b64c4400466fc40a5c3b9868ac648d2ad
Rafael Aguiar <rafael.a1@hotmail.com>
2022-11-27 07:37:00 -0300

    feat: ensure return correct data when run find

--- src/infra/db/mongodb/launch-repository/lauch.ts
+++ src/infra/db/mongodb/launch-repository/lauch.ts
@@ -1,5 +1,6 @@
 import { AddLaunchRepository } from '../../../../data/protocols/launch-repository'
 import { LaunchModel } from '../../../../domain/models/launch'
+import { ResultLaunchPageModel } from '../../../../domain/models/resultLaunchPage'
 import { AddLaunchModel } from '../../../../domain/usecases/add-launch'
 import { FindConditionsModel } from '../../../../domain/usecases/find-launch'
 import { MongoHelper } from '../helpers/mongo-helper'
@@ -17,17 +18,41 @@ export class LaunchMongoRepository implements AddLaunchRepository {
     return result
   }
 
-  async find (conditions: FindConditionsModel): Promise<LaunchModel> {
-    const page = conditions.page || 0
+  async find (conditions: FindConditionsModel): Promise<ResultLaunchPageModel> {
+    const page = Number(conditions.page) || 0
     const launchesPerPage = Number(conditions.limit)
     const search = conditions.search
+    let hasNext = false
+    let hasPrev = false
 
     const launchCollection = await MongoHelper.getCollection('launches')
-    const cursor = await launchCollection.find({
+    let cursor = await launchCollection.find({
       name: { $regex: `(?i)^${search}` }
-    }).limit(launchesPerPage).skip(page * launchesPerPage)
-    const allLaunches = await cursor.toArray()
-    console.log(allLaunches.length)
-    return allLaunches
+    }).toArray()
+
+    const totalDocs = (cursor.length)
+
+    cursor = await launchCollection.find({
+      name: { $regex: `(?i)^${search}` }
+    }).limit(launchesPerPage)
+      .skip(page * launchesPerPage)
+
+    const results = await cursor.toArray()
+    const totalPages = totalDocs / launchesPerPage
+    if (page < totalPages){
+      hasNext = true
+    }
+    if (page !== 0){
+      hasPrev = true
+    }
+
+    return {
+      results,
+      totalDocs,
+      page,
+      totalPages,
+      hasNext,
+      hasPrev
+    }
   }
 }
