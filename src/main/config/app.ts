422036aa55183bd227c99f9e0fec985ab5d37eb2
Rafael Aguiar <rafael.a1@hotmail.com>
2022-11-28 07:25:41 -0300

    chore: add swagger

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -2,8 +2,12 @@ import express from 'express'
 import setupMiddlewares from './middlewares'
 import setupRoutes from './routes'
 
+import swaggerUi from 'swagger-ui-express'
+import swaggerFile from '../../swagger.json'
+
 const app = express()
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 setupMiddlewares(app)
 setupRoutes(app)
 
--- src/utils/rockets/insertRocketsInBD.spec.ts
+++
@@ -1,8 +0,0 @@
-import { InsertRocketsInDB } from './insertRocketsInBD'
-
-describe('Insert Rockets In BD', () => {
-  test('Should return 200 if load data success', async () => {
-    const sut = new InsertRocketsInDB()
-    await sut.run()
-  })
-})
