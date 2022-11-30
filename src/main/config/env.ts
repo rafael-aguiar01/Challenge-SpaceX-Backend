117a202c5ce7a83d06554166603ddd450382cd4f
Rafael Aguiar <rafael.a1@hotmail.com>
2022-11-30 07:26:11 -0300

    fix: change localhost to 127.0.0.1

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,6 +1,6 @@
 export default {
   rocketsLink: 'https://api.spacexdata.com/v4/rockets',
   launchesLink: 'https://api.spacexdata.com/v5/launches',
-  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/spacexapi',
+  mongoUrl: process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/spacexapi',
   port: process.env.PORT || 5050
 }
